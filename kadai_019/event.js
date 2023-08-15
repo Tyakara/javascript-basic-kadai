963f03e20696105d544136a9e00a78f2f838e1cb
Tyakara <141602744+Tyakara@users.noreply.github.com>
2023-08-16 04:05:54 +0900

    Update event.js

--- kadai_019/event.js
+++ kadai_019/event.js
@@ -2,8 +2,8 @@ const btn = document.getElementById("btn");
 
 btn.addEventListener("click", () => {
 
-const text = document.getElementById("text");
+  const text = document.getElementById("text");
 
-text.textContent = "ボタンをクリックしました";
+  text.textContent = "ボタンをクリックしました";
 
 });
